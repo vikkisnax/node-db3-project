649ce1719d99b246f60db0fda181b94826ce7bef
Victoria Blancas <vikki6688@gmail.com>
2023-09-27 19:04:57 -0400

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config')
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -7,8 +9,20 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
+  // const schemeId = req.body.id;
+  // const scheme = await db('schemes').where('id', req.params.id).first()
+  // if (scheme){
+  //   next()
+  // }else{
+  //   next({
+  //     status: 404,
+  //     message: `scheme with scheme_id ${schemeId} not found`
+  //   })
+  // }
 }
+
+
+
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -21,6 +35,9 @@ const checkSchemeId = (req, res, next) => {
 const validateScheme = (req, res, next) => {
 
 }
+
+
+
 
 /*
   If `instructions` is missing, empty string or not a string, or
